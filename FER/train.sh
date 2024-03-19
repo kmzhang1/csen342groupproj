a7ae634638133a620aab5132c1d2c0454bf1b107
Sari Elian <selian@wave8-login3.wave.scu.edu>
2024-03-18 22:30:04 -0700

    updated train.sh

--- FER/train.sh
+++ FER/train.sh
@@ -1,15 +1,41 @@
-python train_fer.py --model ResNet18 --kd_T 5 -s 1 --mu 1.0 #seed 1
-python train_fer.py --model ResNet18 --kd_T 5 -s 5 --mu 1.0 #seed 5
-python train_fer.py --model ResNet18 --kd_T 5 --dataset cifar100 -s 7 --mu 1.0 #seed 7
+# Standard Training
+python train.py --model ResNet18 --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset cifar100
+python train.py --model ResNet18 --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset stanford
+python train.py --model ResNet18 --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset tiny
 
-python train_fer.py --model wrn_40_2 --kd_T 5 -s 1 --mu 0.3
-python train_fer.py --model wrn_40_2 --kd_T 5 -s 5 --mu 0.3
-python train_fer.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3
+python train.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3 -i 240 --dataset cifar100
+python train.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3 -i 240 --dataset stanford
+python train.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3 -i 240 --dataset tiny
 
-python train_fer.py --model ShuffleV2 --kd_T 5 -s 1 --mu 0.5
-python train_fer.py --model ShuffleV2 --kd_T 5 -s 5 --mu 0.5
-python train_fer.py --model ShuffleV2 --kd_T 5 -s 7 --mu 0.5
+python train.py --model ShuffleV2 --kd_T 5 -s 7 --mu 0.5 -i 240 --dataset cifar100
+python train.py --model ShuffleV2 --kd_T 5 -s 7 --mu 0.5 -i 240 --dataset stanford
+python train.py --model ShuffleV2 --kd_T 5 -s 7 --mu 0.5 -i 240 --dataset tiny
 
-python train_fer.py --model MobileNetV2 --kd_T 5 -s 1 --mu 0.3
-python train_fer.py --model MobileNetV2 --kd_T 5 -s 5 --mu 0.3
-python train_fer.py --model MobileNetV2 --kd_T 5 -s 7 --mu 0.3
+python train.py --model MobileNetV2 --kd_T 5 -s 1 --mu 0.3 -i 240 --dataset cifar100
+python train.py --model MobileNetV2 --kd_T 5 -s 1 --mu 0.3 -i 240 --dataset stanford
+python train.py --model MobileNetV2 --kd_T 5 -s 1 --mu 0.3 -i 240 --dataset tiny
+
+python train.py --model Efficient --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset cifar100
+python train.py --model Efficient --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset stanford
+python train.py --model Efficient --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset tiny
+
+# FER Training
+python train.py --model ResNet18 --kd_T 5 -s 7 --mu 1.0 -i 2 --dataset cifar100
+python train.py --model ResNet18 --kd_T 5 -s 7 --mu 1.0 -i 2 --dataset stanford
+python train.py --model ResNet18 --kd_T 5 -s 7 --mu 1.0 -i 2 --dataset tiny
+
+python train.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3 -i 2 --dataset cifar100
+python train.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3 -i 2 --dataset stanford
+python train.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3 -i 2 --dataset tiny
+
+python train.py --model ShuffleV2 --kd_T 5 -s 7 --mu 0.5 -i 2 --dataset cifar100
+python train.py --model ShuffleV2 --kd_T 5 -s 7 --mu 0.5 -i 2 --dataset stanford
+python train.py --model ShuffleV2 --kd_T 5 -s 7 --mu 0.5 -i 2 --dataset tiny
+
+python train.py --model MobileNetV2 --kd_T 5 -s 1 --mu 0.3 -i 2 --dataset cifar100
+python train.py --model MobileNetV2 --kd_T 5 -s 1 --mu 0.3 -i 2 --dataset stanford
+python train.py --model MobileNetV2 --kd_T 5 -s 1 --mu 0.3 -i 2 --dataset tiny
+
+python train.py --model Efficient --kd_T 5 -s 7 --mu 1.0 -i 2 --dataset cifar100
+python train.py --model Efficient --kd_T 5 -s 7 --mu 1.0 -i 2 --dataset stanford
+python train.py --model Efficient --kd_T 5 -s 7 --mu 1.0 -i 2 --dataset tiny
