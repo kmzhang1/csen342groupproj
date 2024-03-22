dfdfd067b63772af1d2f9b7db207ea38ff5584bc
jzuluaga02 <114960212+jzuluaga02@users.noreply.github.com>
2024-03-21 17:45:43 -0700

    Update __init__.py

--- FER/models/__init__.py
+++ FER/models/__init__.py
@@ -2,10 +2,14 @@ from .resnetv2 import ResNet18
 from .wrn import wrn_40_2
 from .mobilenetv2 import mobile_half
 from .ShuffleNetv2 import ShuffleV2
+from .GoogleNet import GoogleNetModel
+from .ResNeXt import ResNeXtModel
 
 model_dict = {
     'ResNet18': ResNet18,
     'wrn_40_2': wrn_40_2,
     'MobileNetV2': mobile_half,
     'ShuffleV2': ShuffleV2,
+    'GoogleNet': GoogleNetModel,
+    'ResNeXt': ResNeXtModel
 }
