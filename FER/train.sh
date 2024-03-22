eb786e6da7e423d0f3584809594e41f5ea1fb68c
jzuluaga02 <114960212+jzuluaga02@users.noreply.github.com>
2024-03-21 17:40:59 -0700

    Update train.sh

--- FER/train.sh
+++ FER/train.sh
@@ -7,6 +7,10 @@ python train.py --model GoogleNet --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset cifar1
 python train.py --model GoogleNet --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset stanford
 python train.py --model GoogleNet --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset tiny
 
+python train.py --model ResNeXt --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset cifar100
+python train.py --model ResNeXt --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset stanford
+python train.py --model ResNeXt --kd_T 5 -s 7 --mu 1.0 -i 240 --dataset tiny
+
 python train.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3 -i 240 --dataset cifar100
 python train.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3 -i 240 --dataset stanford
 python train.py --model wrn_40_2 --kd_T 5 -s 7 --mu 0.3 -i 240 --dataset tiny
